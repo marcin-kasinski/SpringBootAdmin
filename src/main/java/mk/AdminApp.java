e4f81cc464e9f640c6f6299b6dc36c139ff332c3
marcin <marcin.kasinski@gmail.com>
2019-02-15 16:53:20 +0100

    add spring-cloud-starter-kubernetes

--- src/main/java/mk/AdminApp.java
+++ src/main/java/mk/AdminApp.java
@@ -1,12 +1,10 @@
 package mk;
 
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,14 +12,10 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
-//@EnableDiscoveryClient
-//@EnableScheduling
+@EnableDiscoveryClient
+@EnableScheduling
 @Configuration
 public class AdminApp {
-	
-	
-	
-
 	public static void main(String[] args) {
 
 		System.out.println("Version 02.02.2019");
