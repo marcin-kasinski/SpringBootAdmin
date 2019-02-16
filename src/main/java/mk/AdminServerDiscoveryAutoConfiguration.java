eaeeb9bf724741d0face0f97bb02552b73b9b756
marcin <marcin.kasinski@gmail.com>
2019-02-16 14:18:01 +0100

    change spring version to 1.5.19

---
+++ src/main/java/mk/AdminServerDiscoveryAutoConfiguration.java
@@ -0,0 +1,20 @@
+package mk;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import de.codecentric.boot.admin.server.cloud.discovery.DefaultServiceInstanceConverter;
+
+public class AdminServerDiscoveryAutoConfiguration {
+/*
+	public AdminServerDiscoveryAutoConfiguration() {
+		// TODO Auto-generated constructor stub
+	}
+	@Bean
+
+    @ConfigurationProperties(prefix = "spring.boot.admin.discovery.converter")
+    public DefaultServiceInstanceConverter serviceInstanceConverter() {
+        return new DefaultServiceInstanceConverter();
+}
+*/
+}
