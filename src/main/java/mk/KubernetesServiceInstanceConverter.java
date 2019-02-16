d093bad85ee709b32595ed3315facee45989ac60
marcin <marcin.kasinski@gmail.com>
2019-02-16 19:52:24 +0100

    add additional classes

---
+++ src/main/java/mk/KubernetesServiceInstanceConverter.java
@@ -0,0 +1,19 @@
+package mk;
+
+import java.net.URI;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.kubernetes.discovery.KubernetesServiceInstance;
+import org.springframework.util.Assert;
+
+
+public class KubernetesServiceInstanceConverter extends DefaultServiceInstanceConverter {
+
+    @Override
+    protected URI getHealthUrl(ServiceInstance instance) {
+        Assert.isInstanceOf(KubernetesServiceInstance.class,
+                            instance,
+                            "serviceInstance must be of type KubernetesServiceInstance");
+        return ((KubernetesServiceInstance) instance).getUri();
+    }
+}
