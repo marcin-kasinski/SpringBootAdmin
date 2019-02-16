b9eaa8f35c7ef5a5b99df2963fa310751ff5800a
marcin <marcin.kasinski@gmail.com>
2019-02-16 20:23:54 +0100

    add AdminServerDiscoveryAutoConfiguration class

--- src/main/java/mk/AdminServerDiscoveryAutoConfiguration.java
+++ src/main/java/mk/AdminServerDiscoveryAutoConfiguration.java
@@ -1,20 +1,59 @@
 package mk;
 
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import de.codecentric.boot.admin.server.config.AdminServerAutoConfiguration;
+import de.codecentric.boot.admin.server.config.AdminServerMarkerConfiguration;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.services.InstanceRegistry;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.AsyncTaskExecutor;
 
-import de.codecentric.boot.admin.server.cloud.discovery.DefaultServiceInstanceConverter;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@ConditionalOnSingleCandidate(DiscoveryClient.class)
+@ConditionalOnBean(AdminServerMarkerConfiguration.Marker.class)
+@ConditionalOnProperty(prefix = "spring.boot.admin.discovery", name = "enabled", matchIfMissing = true)
+@AutoConfigureAfter(value = AdminServerAutoConfiguration.class, name = {
+    "org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientAutoConfiguration",
+    "org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration"})
 public class AdminServerDiscoveryAutoConfiguration {
-/*
-	public AdminServerDiscoveryAutoConfiguration() {
-		// TODO Auto-generated constructor stub
-	}
-	@Bean
 
-    @ConfigurationProperties(prefix = "spring.boot.admin.discovery.converter")
-    public DefaultServiceInstanceConverter serviceInstanceConverter() {
-        return new DefaultServiceInstanceConverter();
-}
-*/
+    @Bean
+    @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "spring.boot.admin.discovery")
+    public InstanceDiscoveryListener instanceDiscoveryListener(ServiceInstanceConverter serviceInstanceConverter,
+                                                               DiscoveryClient discoveryClient,
+                                                               InstanceRegistry registry,
+                                                               InstanceRepository repository) {
+        InstanceDiscoveryListener listener = new InstanceDiscoveryListener(discoveryClient, registry, repository);
+        listener.setConverter(serviceInstanceConverter);
+        return listener;
+    }
+
+    @Configuration
+    @ConditionalOnMissingBean({ServiceInstanceConverter.class})
+    @ConditionalOnBean(KubernetesClient.class)
+    public static class KubernetesConverterConfiguration {
+        @Bean
+        @ConfigurationProperties(prefix = "spring.boot.admin.discovery.converter")
+        public KubernetesServiceInstanceConverter serviceInstanceConverter() {
+            return new KubernetesServiceInstanceConverter();
+        }
+    }
 }
